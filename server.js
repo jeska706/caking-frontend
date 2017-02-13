aad51aeeb1279f1fbfa7387d2c52545a3907e7da
jeska706 <jessicabeardsley@hotmail.com>
2017-02-13 09:29:45 -0700

    Commented out mongoose for frontend, no current link to heroku

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var mongoose = require('mongoose');
 var port    = 2043 || process.env.PORT;
 
 
-var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/cakin_it_app_frontend';
-mongoose.connect(mongoUri);
-
-mongoose.connection.once('open', function(){
-    console.log('connected to mongod');
-});
+// var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:20717/cakin_it_app_frontend';
+// mongoose.connect(mongoUri);
+//
+// mongoose.connection.once('open', function(){
+//     console.log('connected to mongod');
+// });
 
 app.use(express.static('public'));
 
